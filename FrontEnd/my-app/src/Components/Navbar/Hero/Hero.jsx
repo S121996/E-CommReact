f93038d8c90b01446e71a61a5c21b41a6c88cd44
DrumKit <sumitbhargav12@gmail.com>
2024-05-23 15:00:29 +0530

    new components Popular

--- FrontEnd/my-app/src/Components/Navbar/Hero/Hero.jsx
+++ FrontEnd/my-app/src/Components/Navbar/Hero/Hero.jsx
@@ -9,9 +9,9 @@ const Hero = () => {
   return (
     <div className='hero'>
         <div className='hero-left'>
-            <h2>New Arrivals only</h2>
+            <h2>NEW ARRIVALS ONLY</h2>
             <div>
-                <div className='hand-hand-icon'>
+                <div className='hero-hand-icon'>
                    <p>new</p>
                    <img src={hand_icon} alt=""/>
                 </div>
