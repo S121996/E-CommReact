1e46a27efd4e5ce2fc4f2e7a823074a915118122
DrumKit <sumitbhargav12@gmail.com>
2024-05-27 14:39:39 +0530

    Creating Product details

--- FrontEnd/my-app/src/Pages/LoginSignUp.jsx
+++ FrontEnd/my-app/src/Pages/LoginSignUp.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import './CSS/LoginSignUp.css'
 
 const LoginSignUp = () => {
   return (
-    <div>LoginSignUp</div>
-  )
-}
+    <div className="loginsignup">
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Enter Your mail id" />
+          <input type="password" placeholder="Enter Your Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an account?
+          <span>Login here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name='' id=''/>
+        <p>By continuing,i agree to the terms of use & privacy.</p>
 
-export default LoginSignUp
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginSignUp;
--- FrontEnd/my-app/src/Pages/Product.jsx
+++ FrontEnd/my-app/src/Pages/Product.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import React, { useContext,useParams } from 'react'
+import { ShopContext } from '../Context/ShopContext'
+import Breadcrums from '../Components/Breadcrums/Breadcrums';
+import ProductDisplay from '../Components/ProductDisplay/ProductDisplay';
 
 const Product = () => {
+ const{all_product} =useContext(ShopContext)
+ const {productId} =useParams();
+ const product =all_product.find((e)=>e.id===Number(productId))
   return (
-    <div>Product</div>
+    <div className='product'>
+    <Breadcrums product={product}/>
+    <ProductDisplay product={product}/>
+    </div>
   )
 }
 
