8c83fcbe319172da1a0b845a0df10873ad33a37e
DrumKit <sumitbhargav12@gmail.com>
2024-05-29 22:21:06 +0530

    LoginSignUP

--- FrontEnd/my-app/src/Pages/ShopCategory.jsx
+++ FrontEnd/my-app/src/Pages/ShopCategory.jsx
@@ -22,7 +22,7 @@ const ShopCategory = (props) => {
       </div>
       <div className="shopcategory-products">
         {all_product.map((item,i) => {
-          if (props.category===item.category) {
+          if (props.Category===item.Category) {
             return (
               <Item
                 key={i}
